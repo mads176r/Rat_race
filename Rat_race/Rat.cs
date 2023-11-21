107c172bda3f0a39991d8accb1a8bd9620441fd8
Mads Højte Rasmussen <mads176r@edu.sde.dk>
2023-11-21 09:42:14 +0100

    Færdig med Rat

--- Rat_race/Rat.cs
+++ Rat_race/Rat.cs
@@ -2,18 +2,41 @@
 {
     public class Rat
     {
-        public string name;
+        public string Name;
         private int _position;
-        public int Position {get;set; }
+        public int Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                if (_position != value)
+                {
+                    _position = value;
+                }
+            }
+        }
 
         public void ResetRat()
         {
-            return;
+            _position = 0;
+
+            Position = 0;
         }
 
         public int MoveRat()
         {
-            return _position;
+            int upper = 10;
+            int lower = 0;
+
+            RNG randomMove = new RNG();
+            int move = randomMove.Range(upper, lower);
+
+            int newPosition = _position + move;
+
+            return newPosition;
         }
     }
 
