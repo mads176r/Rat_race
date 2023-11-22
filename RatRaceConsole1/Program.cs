f11e940d6593bc0fc01514471d3aa4a3d3225061
Ikhvan Lom-Alievich Bajseluev <ikhv0002@edu.sde.dk>
2023-11-22 14:08:06 +0100

    ændret tekst til engelsk og er begyndt på at oprette så man kan placere bets

--- RatRaceConsole1/Program.cs
+++ RatRaceConsole1/Program.cs
@@ -32,19 +32,19 @@ namespace RatRaceConsole1
 
             // bruger indtaster login
 
-            Console.WriteLine("Indtast Brugernanv");
+            Console.WriteLine("Insert username");
 
             string username = Console.ReadLine();
 
-            Console.WriteLine("Indtast password");
+            Console.WriteLine("Insert password");
 
             string password = Console.ReadLine();
 
             // Test om password eksisterer i JSON FILER
             while (racemanager.LoginToPlayer(username, password) == null)
             {
-                Console.WriteLine("Prøv igen");
-                Console.Write("Brugernavn: ");
+                Console.WriteLine("Try again");
+                Console.Write("Username: ");
                 username = Console.ReadLine();
                 Console.Write("Password: ");
                 password = Console.ReadLine();
@@ -52,48 +52,64 @@ namespace RatRaceConsole1
 
             //Velkommen hvad du gerne lave hva?
 
-            Console.WriteLine("Velkommen vælg en mulighed her. Efterfulgt af Enter");
-            Console.WriteLine("1: place bet");
-            Console.WriteLine("2: Conduct Race");
-            Console.WriteLine("3: Create Track");
-            Console.WriteLine("4: Create Rat");
-            Console.WriteLine("5: Start Game");
-            Console.WriteLine("6: End Game");
+            Console.WriteLine("Welcome choose an option and press enter afterwards");
+            Console.WriteLine("1: Place Bet");
+            Console.WriteLine("2: Create Track");
+            Console.WriteLine("3: Create Rat");
+            Console.WriteLine("4: Start Game");
+            Console.WriteLine("5: End Game");
+
 
             int choice = int.Parse(Console.ReadLine());
-            switch(choice)
+            switch (choice)
             {
                 case 1:
-                    Console.WriteLine("Her placere du dit bet");
+                    Console.WriteLine("choose your rat");
+                    Console.WriteLine("1 ");
+                    Console.WriteLine("2 ");
+                    Console.WriteLine("3 ");
+                    Console.WriteLine("4" );
+                    Console.WriteLine("5" );
+                    Console.WriteLine("6 ");
+
+                    int betChoice = int.Parse(Console.ReadLine());
+
                     break;
+
 
                 case 2:
-
-                    break;
-
-                case 3:
                     Track track = new Track();
 
-                    Console.Write("Indsæt et navn: ");
+                    Console.Write("Give the track a name: ");
                     string trackName = Console.ReadLine();
-                    Console.Write("Angiv længde: ");
+                    Console.Write("Choose a length: ");
                     int trackLength = int.Parse(Console.ReadLine());
 
                     track = manager.CreateTrack(trackName, trackLength);
                     ratRaceRepository.Save(manager.Tracks);
                     break;
 
-                case 4:
+                case 3:
                     Rat rat = new Rat();
-                    Console.Write("Giv rotten et navn: ");
+                    Console.Write("Give the rat a name: ");
                     string ratName = Console.ReadLine();
                     rat = manager.CreateRat(ratName);
 
                     ratRaceRepository.Save(manager.Rats);
                     break;
 
+                case 4:
+                    Console.WriteLine("The game starts!");
+                    
+                    break;
 
-                
+
+                case 5:
+                    Environment.Exit(0); // Lukker console applikationen
+                    break;
+
+
+
             }
         }
 
