ef10cafc53768052cec1db7d145c5f20ac43197f
Ikhvan Lom-Alievich Bajseluev <ikhv0002@edu.sde.dk>
2023-11-29 10:13:18 +0100

    sql connection

---
+++ Rat_race/SqlConnectionManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Rat_race
+{
+    public class SqlConnectionManager
+    {
+        string connectionString;
+
+        public SqlConnectionManager(string dataSource, string initialCatalog, 
+                                    bool integratedSecurity = true,  bool trustServerCertificate = true)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+            {
+                DataSource = dataSource,
+                InitialCatalog = initialCatalog,
+                IntegratedSecurity = integratedSecurity,
+                TrustServerCertificate = trustServerCertificate
+            };
+
+            connectionString = builder.ConnectionString;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+
+        public void OpenConnection(SqlConnection connection)
+        {
+                connection.Open();
+        }
+
+        public void CloseConnection(SqlConnection connection)
+        {
+                connection.Close();
+        }
+    }
+}
